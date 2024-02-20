642780d1c37e38743e453851a91fc4fe4a4a1dc6
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-02-20 19:25:36 +0100

    register events

--- src/main/java/de/dertoaster/movecraft/bridges/MovecraftBridges.java
+++ src/main/java/de/dertoaster/movecraft/bridges/MovecraftBridges.java
@@ -2,6 +2,16 @@ package de.dertoaster.movecraft.bridges;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import de.dertoaster.movecraft.bridges.listeners.SignInteractListener;
+
 public class MovecraftBridges extends JavaPlugin {
 
+	@Override
+	public void onEnable() {
+		super.onEnable();
+		
+		// Register event listener
+		this.getServer().getPluginManager().registerEvents(new SignInteractListener(), this);
+	}
+	
 }
